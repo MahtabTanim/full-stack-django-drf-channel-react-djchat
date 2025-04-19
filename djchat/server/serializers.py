13c57c476db906e1b2980781c3e5f5a35923e41f
MahtabTanim <tanimbsmrstu@gmail.com>
2025-04-19 22:25:11 +0600

    27 docstring

--- djchat/server/serializers.py
+++ djchat/server/serializers.py
@@ -1,8 +1,37 @@
 from rest_framework import serializers
-from .models import Server
+from .models import Server, Channel
+
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = "__all__"
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    channel_server = ChannelSerializer(many=True)
+    total_members = serializers.SerializerMethodField()
+
     class Meta:
         model = Server
-        fields = ["id", "name", "category", "description"]
+        exclude = [
+            "member",
+        ]
+        # fields = [
+        #     "id",
+        #     "name",
+        #     "category",
+        #     "description",
+        #     "channel_server",
+        #     "total_members",
+        # ]
+
+    def get_total_members(self, obj):
+        return obj.member.count()
+
+    # def to_representation(self, instance):
+    #     data = super().to_representation(instance)
+    #     total_members = self.context.get("total_members")
+    #     if not total_members:
+    #         data.pop("total_members", None)
+    #     return data
--- djchat/server/views.py
+++ djchat/server/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Server
 from .serializers import ServerSerializer
-
+from rest_framework.exceptions import AuthenticationFailed
+from django.db.models import Count
 
 # Create your views here.
 
@@ -13,8 +14,36 @@ class ServerListViewSet(ViewSet):
     def list(self, request):
         servers = Server.objects.all()
         category = request.GET.get("category")
+        by_user = request.query_params.get("by_user") == "true"
+        qty = request.query_params.get("qty")
+        # total_members = request.query_params.get("total_members") == "true"
+        if by_user and not request.user.is_authenticated:
+            raise AuthenticationFailed()
         if category:
             print(category)
             servers = servers.filter(category__name=category)
-        data = ServerSerializer(servers, many=True).data
+            print(servers)
+        if by_user:
+            user_id = request.user.id
+            servers = servers.filter(member=user_id)
+        # if total_members:
+        #     servers = servers.annotate(total_members=Count("member"))
+        if qty:
+            servers = servers[: int(qty)]
+
+        data = ServerSerializer(
+            servers,
+            many=True,
+            # context={"total_members": total_members,}
+        ).data
         return Response(data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        if not request.user.is_authenticated:
+            raise AuthenticationFailed()
+        try:
+            pk = int(pk)
+            server = Server.objects.get(pk=pk)
+            return Response(ServerSerializer(server).data, status=status.HTTP_200_OK)
+        except (ValueError, Server.DoesNotExist):
+            return Response({"detail": "not found"}, status=status.HTTP_404_NOT_FOUND)
